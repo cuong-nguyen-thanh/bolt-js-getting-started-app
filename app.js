a6c7a45aff598b762955db67f5328d48ef2e5a9e
Nguyễn Thanh Cương <nt.cuong3@samsung.com>
2021-01-07 10:49:40 +0700

    Add Procfile

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const { App } = require('@slack/bolt');
-
+const SLACK_TOKEN = process.env.SLACK_BOT_TOKEN || 'xoxb-1564172303667-1578673961893-0kEfc51gPuan5yGddv3f3lDl';
+const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET || '7bac532792e95a6f98a39a44cdfbd1bd';
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  token: SLACK_TOKEN,
+  signingSecret: SLACK_SIGNING_SECRET
 });
 
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
+  console.log('adadadadada')
   // say() sends a message to the channel where the event was triggered
   await say({
     blocks: [
